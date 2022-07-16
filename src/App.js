b7bbf3807eb9f6f59167c702c2d8ec3a2a68b409
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-07-16 13:26:03 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { request } from "graphql-request";
-import { API_URL } from "./.env";
-import Post from "./components/Post";
-import Header from "./components/header/Header";
-
-const QUERY = `{
-    posts {
-      id
-      slug
-      title
-      content
-      creationDate
-      coverPhoto{
-        url
-      }
-      author {
-        id
-        nickname
-        avatar{
-            url
-        }
-      }
-    }
-  }`;
-
-export const fetchData = async (setData) => {
-    const { posts } = await request(API_URL, QUERY);
-
-    setData(posts);
-};
+import React from "react";
 
 function App() {
-    const [posts, setPosts] = useState(null);
-
-    useEffect(() => {
-        fetchData(setPosts);
-    }, []);
 
     return (
         <div className="App">
-            <Header />
-
-            {posts &&
-                posts.map((post) => (
-                    <Post
-                        title={post.title}
-                        content={post.content}
-                        key={post.id}
-                    />
-                ))}
+            works
         </div>
     );
 }
