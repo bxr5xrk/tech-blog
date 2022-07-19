db1e96050109e0b75bb9d6e4fe68ce2b74adf2ab
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-07-19 14:58:45 +0300

    fixed bug

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getPost } from "../../api/PostService";
 import st from "./PostItem.module.css";
-
 
 const PostItem = () => {
     let { id } = useParams();
     const [post, setPost] = useState({});
 
-    useState(() => {
-        getPost(id, setPost)
-    }, []);
-    
+    useEffect(() => {
+        getPost(id, setPost);
+    }, [id]);
 
     return (
         <article className={st.wrapper}>
-            <img src={post.coverImageUrl} alt="coverImage" className={st.image} />
+            <img
+                src={post.coverImageUrl}
+                alt="coverImage"
+                className={st.image}
+            />
             <h1 className={st.title}>{post.title}</h1>
             <p className={st.content}>{post.content}</p>
         </article>
